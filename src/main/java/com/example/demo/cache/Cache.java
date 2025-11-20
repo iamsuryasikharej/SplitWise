f762711eb8aedfe1b32a286f6484569960e9bcf7
Surya <surya@Suryas-MacBook-Air.local>
2025-11-20 19:27:14 +0530

    implementing custom cache

---
+++ src/main/java/com/example/demo/cache/Cache.java
@@ -0,0 +1,24 @@
+package com.example.demo.cache;
+
+import com.google.common.util.concurrent.Futures;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+public class Cache<K, V> {
+
+    Map<K, V> cache = new HashMap<>();
+
+    public Future<V> set(K k, V v) {
+        return CompletableFuture.supplyAsync(() -> {
+            cache.put(k, v);
+            return cache.get(k);
+        });
+    }
+
+    public Future<V> get(K k) {
+        return CompletableFuture.supplyAsync(() -> cache.get(k));
+    }
+}
